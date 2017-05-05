391d00034512b2156ebb6423e2bddf6f8f54e701
Mikhail Foenko <mfoenko@gmail.com>
2017-05-05 09:45:40 -0400

    first route for led controller

--- app.js
+++ app.js
@@ -5,14 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
+var led = require('./lights.js');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,13 +16,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use(led);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -42,5 +36,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+*/
 module.exports = app;
--- lights.js
+++ lights.js
@@ -1,28 +1,45 @@
 var pi = require('pi-fast-gpio');
+var app = express();
+var router = app.Router();
 
 pi = new pi();
 
-const R = 10;
-const G = 9;
-const B = 11;
+const R_PIN = 10;
+const G_PIN = 9;
+const B_PIN = 11;
 
+var R_VAL = 0;
+var G_VAL = 0;
+var B_VAL = 0;
 
 pi.connect('127.0.0.1', 8888, function(e){
-
-	console.log(e);
-
-	while(true){
-	for(var b=0;b<256;b++)	
-	for(var i = 11;i>=9;i--){
-		pi.setPwmDutycycle(i,b);
-	}
-
-	for(var b=255;b>=0;b--){
-	pi.setPwmDutycycle(R,b)
-	pi.setPwmDutycycle(G,b)
-	pi.setPwmDutycycle(B,b)
-	}
-	}
-	pi.close();
-	
+	console.log("Pi Connect", e);
 })
+
+router.put("/led", function(req, res, next){
+
+	var r = req.body.r;
+	var g = req.body.g;
+	var b = req.body.b;
+
+	if(!checkColor(r) || !checkColor(g) || !checkColor(b)){
+		res.send(400);
+		return next();
+	}
+
+	R_VAL = r;
+	G_VAL = g;
+	B_VAL = b;
+
+	pi.setPwmDutycycle(R_PIN,R_VAL)
+	pi.setPwmDutycycle(G_PIN,G_VAL)
+	pi.setPwmDutycycle(B_PIN,B_VAL)
+	
+});
+
+function checkColor(c){
+	return Number.isInteger(c) && c >= 0 && c <256;
+}
+
+
+module.exports = router;
